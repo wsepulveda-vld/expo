564b7a16d4d7655cf61f579a5f5fea311d1ebedd
Nick Novitski <nick@expo.io>
2018-10-23 08:32:38 -0700

    android: Create shell.nix, use in CI

--- default.nix
+++ default.nix
@@ -1,15 +1,24 @@
 let
   pin = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
 
+  # DONTMERGE
+  #nixpkgs = builtins.fetchTarball {
+  #  url = "https://github.com/NixOS/nixpkgs-channels/archive/${pin.rev}.tar.gz";
+  #  inherit (pin) sha256;
+  #};
   nixpkgs = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs-channels/archive/${pin.rev}.tar.gz";
-    inherit (pin) sha256;
+    url = "https://github.com/nicknovitski/nixpkgs/archive/android-sdk-26-licenses.tar.gz";
+    sha256 = "1c52sksgla3fprb2xrw47992jx3lphcr5wqs51xnl607dimflxgm";
   };
+
 
 in
 
 { ... } @ args:
 
   import nixpkgs (args // {
-    config = { };
+    config = {
+      allowUnfree = true;
+      android_sdk.accept_license = true;
+    };
   })
