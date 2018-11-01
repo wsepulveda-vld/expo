dd66fd500955b04a32ad65691d9c92a42c46b98a
Nick Novitski <nick@expo.io>
2018-10-31 18:26:15 -0700

    break on purpose

--- default.nix
+++ default.nix
@@ -1,16 +1,10 @@
 let
   pin = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
 
-  # DONTMERGE
-  #nixpkgs = builtins.fetchTarball {
-  #  url = "https://github.com/NixOS/nixpkgs-channels/archive/${pin.rev}.tar.gz";
-  #  inherit (pin) sha256;
-  #};
   nixpkgs = builtins.fetchTarball {
-    url = "https://github.com/nicknovitski/nixpkgs/archive/android-sdk-26-licenses.tar.gz";
-    sha256 = "1c52sksgla3fprb2xrw47992jx3lphcr5wqs51xnl607dimflxgm";
+    url = "https://github.com/NixOS/nixpkgs-channels/archive/${pin.rev}.tar.gz";
+    inherit (pin) sha256;
   };
-
 
 in
 
